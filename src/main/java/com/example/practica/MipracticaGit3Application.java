0fd05c10a476bf179e09b3bcc0bc92ab335503e7
Fernando Moutinho <moutinho132@gmail.com>
2024-01-25 11:34:07 -0400

    mi primer commit

---
+++ src/main/java/com/example/practica/MipracticaGit3Application.java
@@ -0,0 +1,13 @@
+package com.example.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MipracticaGit3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MipracticaGit3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practica/MipracticaGit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MipracticaGit3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
